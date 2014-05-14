14420cb386bd3fe9b0cd3c62db920774e02ee00c
Russ <russjohnson09@gmail.com>
2014-05-14 00:20:34 -0500

    adding node-static to depend

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var hb = require('express3-handlebars');
+var http = require("http");
 
 app.engine('hbs', hb({extname:'hbs',defaultLayout:"main.hbs"}));
 app.set('view engine', 'hbs');
@@ -10,6 +11,17 @@ app.get('/', function(req, res){
 });
 
 app.listen(3000);
+
+
+//static server
+var file = new require('node-static').Server('./public');
+
+http.createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();
+}).listen(3000);
+
 
 
 var greetings = ["Hello","こんにちは","夜露死苦"]
