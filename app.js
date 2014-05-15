d974cbc944ca204ad75607b14fcc7c36b5dfed24
Russ <russjohnson09@gmail.com>
2014-05-14 22:01:23 -0500

    static in /public main site greetings

--- app.js
+++ app.js
@@ -6,23 +6,12 @@ var http = require("http");
 app.engine('hbs', hb({extname:'hbs',defaultLayout:"main.hbs"}));
 app.set('view engine', 'hbs');
 
-app.get('/handlebars', function(req, res){
+app.get('/greetings', function(req, res){
   res.render('index',{greeting:randElement(greetings),title:req.url});
 });
 
-app.use(express.static(__dirname + "/public_html"));
+app.use("/public",express.static(__dirname + "/public_html"));
 app.listen(3000);
-
-var static = require('node-static');
-var file = new static.Server('./public');
-
-http.createServer(function (request, response) {
-    request.addListener('end', function () {
-        file.serve(request, response);
-    }).resume();
-}).listen(8080);
-
-
 
 var greetings = ["Hello","こんにちは","夜露死苦"]
 
