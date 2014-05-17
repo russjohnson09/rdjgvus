e6f923f354cd1151a0f79e78f775ff8f69f17efa
Russ <russjohnson09@gmail.com>
2014-05-16 21:25:05 -0500

    iya, mongodb

--- mongo.js
+++ mongo.js
@@ -1,1 +1,20 @@
-var mongo = require("mongo");
+var m = require("mongodb");
+var Db = m.Db,
+MongoClient = m.MongoClient,
+Server = m.Server,
+ReplSetServers = m.ReplSetServers,
+ObjectID = m.ObjectID,
+Binary = m.Binary,
+GridStore = m.GridStore,
+Grid = m.Grid,
+Code = m.Code,
+BSON = m.pure().BSON,
+assert = require('assert');
+
+var db = new Db('test', new Server('localhost', 27017));
+
+db.open(function(err, db) {
+    assert.equal(null, err);
+    db.on('close',test.done.bind(test));
+    db.close();
+});
