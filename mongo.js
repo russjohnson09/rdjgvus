c917d7580498f46705e91c4fdf2fe999951a2f9a
Russ <russjohnson09@gmail.com>
2014-05-17 17:38:09 -0500

    keep connection to db open

--- mongo.js
+++ mongo.js
@@ -11,14 +11,22 @@ Code = m.Code,
 BSON = m.pure().BSON,
 assert = require('assert');
 
+var logcollection;
+
 var db = new Db('test', new Server('localhost', 27017),{safe:false});
 
 db.open(function(err, db) {
-    var collection = db.collection("test");
-    collection.insert({a:2},{w:1}, function(err, result) {
-        console.log(err);
-        console.log(result);
-        db.close();
-    });
+    logcollection = db.collection("logcollection");
 });
 
+
+require("http").createServer(function(req,res){
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || 
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
+    logcollection.insert({a:ip},{w:1}, function(err, result) {
+        console.log(err);
+        console.log(result);
+        res.end("success");
+    });
+}).listen(3000);
