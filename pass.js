c0908b9d1cde736be228894b3da74a4e6ff5eb66
Russ <russjohnson09@gmail.com>
2014-05-16 20:37:42 -0500

    things

--- pass.js
+++ pass.js
@@ -1,67 +1,92 @@
-// app/routes.js
-module.exports = function(app, passport) {
+var express = require('express');
+var app = express();
+var hb = require('express3-handlebars');
+var http = require("http");
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
 
-	// =====================================
-	// HOME PAGE (with login links) ========
-	// =====================================
-	app.get('/', function(req, res) {
-		res.render('index.ejs'); // load the index.ejs file
+mongoose.connect('localhost:27017/test');
+
+app.engine('hbs', hb({extname:'hbs',defaultLayout:"empty.hbs"}));
+app.set('view engine', 'hbs');
+//app.use(express.logger('dev'));
+//app.use(express.cookieParser());
+//app.use(express.session({ secret: 'russ' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.get('/', function(req, res) {
+	res.render('auth');
+});
+app.get('/login', function(req, res) {
+	res.render('login', { message: req.flash('loginMessage') }); 
+});
+app.get('/signup', function(req, res) {
+	res.render('signup', { message: req.flash('signupMessage') });
+});
+
+app.get('/profile', isLoggedIn, function(req, res) {
+	res.render('profile', {
+		user : req.user
 	});
+});
 
-	// =====================================
-	// LOGIN ===============================
-	// =====================================
-	// show the login form
-	app.get('/login', function(req, res) {
-
-		// render the page and pass in any flash data if it exists
-		res.render('login.ejs', { message: req.flash('loginMessage') }); 
-	});
-
-	// process the login form
-	// app.post('/login', do all our passport stuff here);
-
-	// =====================================
-	// SIGNUP ==============================
-	// =====================================
-	// show the signup form
-	app.get('/signup', function(req, res) {
-
-		// render the page and pass in any flash data if it exists
-		res.render('signup.ejs', { message: req.flash('signupMessage') });
-	});
-
-	// process the signup form
-	// app.post('/signup', do all our passport stuff here);
-
-	// =====================================
-	// PROFILE SECTION =====================
-	// =====================================
-	// we will want this protected so you have to be logged in to visit
-	// we will use route middleware to verify this (the isLoggedIn function)
-	app.get('/profile', isLoggedIn, function(req, res) {
-		res.render('profile.ejs', {
-			user : req.user // get the user out of session and pass to template
-		});
-	});
-
-	// =====================================
-	// LOGOUT ==============================
-	// =====================================
-	app.get('/logout', function(req, res) {
-		req.logout();
-		res.redirect('/');
-	});
-};
-
-// route middleware to make sure a user is logged in
+app.get('/logout', function(req, res) {
+	req.logout();
+	res.redirect('/');
+});
+	
+	
 function isLoggedIn(req, res, next) {
-
-	// if user is authenticated in the session, carry on 
 	if (req.isAuthenticated())
 		return next();
-
-	// if they aren't redirect them to the home page
 	res.redirect('/');
 }
+app.listen(3000);
 
+var LocalStrategy   = require('passport-local').Strategy;
+var User       		= require('./user');
+var passport = require('passport');
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+passport.use('local-signup', new LocalStrategy({
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true
+},
+function(req, email, password, done) {
+    process.nextTick(function() {
+    User.findOne({ 'local.email' :  email }, function(err, user) {
+        // if there are any errors, return the error
+        if (err)
+            return done(err);
+
+        // check to see if theres already a user with that email
+        if (user) {
+            return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+        } else {
+            var newUser            = new User();
+            newUser.local.email    = email;
+            newUser.local.password = newUser.generateHash(password);
+            newUser.save(function(err) {
+                if (err)
+                    throw err;
+                return done(null, newUser);
+            });
+        }
+
+    });    
+
+    });
+
+}));
