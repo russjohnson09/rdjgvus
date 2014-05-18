afbd0196a25b8992f9a0012a177e14907b87d40e
Russ <russjohnson09@gmail.com>
2014-05-18 11:35:07 -0500

    added mongodb, preping for knockout

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ var hb = require('express3-handlebars');
 var http = require("http");
 var config = require("./config");
 
+var m = require("mongodb");
+
+var db = new m.Db('test', new m.Server('localhost', 27017),{safe:false});
+
+db.open(function(err, db) {
+    knockoutCollection = db.collection("knockout");
+});
+
 app.engine('hbs', hb({extname:'hbs',defaultLayout:"main.hbs"}));
 app.set('view engine', 'hbs');
+app.use("/",express.static(__dirname + "/public_html"));
 
 app.get('/greetings', function(req, res){
   res.render('index',{greeting:randElement(greetings),title:req.url});
@@ -23,7 +32,16 @@ app.get("/request",function(req,res){
   res.end("<html><head>" + "<meta charset='UTF-8'>" + "</head><body>" + greeting + "</body></html>");
 });
 
-app.use("/",express.static(__dirname + "/public_html"));
+app.get("/knockout",function(req,res){
+    res.render('knockout',{'greeting':randElement(greetings)});
+});
+
+app.post("/knockout",function(req,res){
+    knockoutCollection.insert({a:1},{w:1}, function(err, result) {
+        res.end("1");
+    });
+});
+
 app.listen(3000);
 
 var greetings = ["Hello","こんにちは","夜露死苦"];
