8c6c9193781e17f72642a2c236629d7251ecb5ee
russ <russjohnson09@gmail.com>
2014-06-16 21:45:46 -0500

    added some documentation

--- public_html/bullethell/game.js
+++ public_html/bullethell/game.js
@@ -1,7 +1,8 @@
-function Game(context, options) {
+function Game(canvas, options) {
     var self = this;
-    self.context = context;
-    if (!context) return;
+    self.canvas = canvas;
+    self.context = canvas.getContext('2d');
+    if (!self.context) return;
     self.margin = options.margin;
     self.width =  options.width;
     self.height = options.height;
@@ -10,9 +11,9 @@ function Game(context, options) {
     self.gameComplete = false; //set to true on completion of all levels
     self.levels = [];  //array of levels
     if (options.debug) {
-        context.canvas.width = self.width + self.margin.x * 2;  //include margin as part of visible screen
-        context.canvas.height = self.height + self.margin.y * 2;
-        context.translate(options.margin.x,options.margin.y);   //origin will start at upper left of game screen.
+        canvas.width = self.width + self.margin.x * 2;  //include margin as part of visible screen
+        canvas.height = self.height + self.margin.y * 2;
+        self.context.translate(options.margin.x,options.margin.y);   //origin will start at upper left of game screen.
         self.trace = 10;  //print trace every nth bullet 
     }
     else {
@@ -29,8 +30,15 @@ function Game(context, options) {
     self.loop = function() {
         var self = this;
         var c = self.context;
-        c.clearRect(-self.margin.x,-self.margin.y,self.width,self.height);
-        c.fillStyle = "#FFF";
+        // Store the current transformation matrix
+        c.save();
+        // Use the identity matrix while clearing the canvas
+        c.setTransform(1, 0, 0, 1, 0, 0);
+        var width = self.canvas.width;
+        var height = self.canvas.height;
+        c.clearRect(0, 0, width, height);
+        // Restore the transform
+        c.restore();
         c.strokeRect(0,0,self.width,self.height);
         if (!self.levels[self.levelIdx].loop()) {
             self.levelIdx++;
@@ -45,6 +53,7 @@ function Game(context, options) {
             if (!success) {
                 self.enemies.splice(i, 1);
             }
+            e.draw(self.context);
         }
     };
     self.oob = function(pos) {
@@ -63,32 +72,34 @@ function Game(context, options) {
 
 //simulate a particle given postion, velocity and acceleration
 function Partical(pos,velocity,acceleration) {
-    this.pos = pos || Vector2();
-    this.velocity = velocity || Vector2();
-    this.acceleration = acceleration || Vector2();
-    this.move = function() {
+    var self = {};
+    self.pos = pos || Vector2();
+    self.velocity = velocity || Vector2();
+    self.acceleration = acceleration || Vector2();
+    self.move = function() {
         var self = this;
         self.pos.add(self.velocity);
         self.velocity.add(self.acceleration);
     };
-    return this;
+    return self;
 }
 
 //2 dimensional vector with addition
 function Vector2(o) {
+    var self = {};
     if (!o) {
-        this.x = 0;
-        this.y = 0;
+        self.x = 0;
+        self.y = 0;
     }
     else {
-        this.x = o.x || 0;
-        this.x = o.y || 0;
+        self.x = o.x || 0;
+        self.y = o.y || 0;
     }
-    this.add = function(v1) {
+    self.add = function(v1) {
         var self = this;
         self.x += v1.x;
         self.y += v1.y;
         return self;
     }
-    return this;
+    return self;
 }
--- public_html/bullethell/level1.js
+++ public_html/bullethell/level1.js
@@ -1,11 +1,11 @@
 function Level1(game) {
-    var self = this;
+    var self = {};
     self.game = game;
     self.frame = 0;
     self.loop = function(){
         var self = this;
-        if (self.frame % 10 == 0) {
-            self.game.addEnemy(basicEnemy(self.game));
+        if (self.frame == 0) {
+            self.game.addEnemy(basicEnemy(self.game,{pos:{x:10,y:10},velocity:{x:1,y:10}}));
         }
         self.frame++;
         return true;
@@ -14,25 +14,39 @@ function Level1(game) {
 }
 
 function basicEnemy(game,opts) {
+    var self = {};
     if (!game) return;
-    this.game = game;
+    self.game = game;
+    self.radius = 5;
+    self.fillStyle = "#000";
     if (!opts) {
-        this.partical = Partical();
+        self.partical = Partical();
     }
     else {
-        this.partical = Partical(Vector2(opts.pos),
-                   Vector2(opts.veloctiy),
+        self.partical = Partical(Vector2(opts.pos),
+                   Vector2(opts.velocity),
                    Vector2(opts.acceleration));
     }
-    this.loop = function() {
+    self.loop = function() {
         var self = this;
-        if (game.oob(partical.pos)) {
+        if (game.oob(self.partical.pos)) {
             return false;
         }
         else {
-            partical.move();
+            self.partical.move();
         }
         return true;
     };
-    return this;
+    self.draw = function(context) {
+        context.fillStyle = self.fillStyle;
+        drawCircle(context,self.partical.pos.x,self.partical.pos.y,self.radius);
+    };
+    return self;
+}
+
+function drawCircle(context,x,y,radius) {
+    context.beginPath();                    //begin path, I think fill might already closepath.
+    context.arc(x,y,radius,0,Math.PI*2);
+    context.closePath();
+    context.fill();
 }
