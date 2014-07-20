0f776b9377c963390ec74f94b8daab4d833a47ca
russ <russjohnson09@gmail.com>
2014-07-19 19:48:10 -0500

    move localized strings to their own file, add smoe more strings

---
+++ public_html/resume/strings.js
@@ -0,0 +1,23 @@
+//localization strings
+
+            
+var i18n = {
+    'eng': {
+        'contact_info': 'Contact Info',
+        'russ_full' : 'Russell Johnson',
+        'education' : 'Education',
+        'programming' : 'Programming',
+        'web' : 'Web',
+        'javascript': 'Javascript',
+        'cplus': 'C++'
+    },
+    'jpn': {
+        'contact_info':'連絡',
+        'russ_full' : 'ラセル=ジオンソン',
+        'education' : '教育',
+        'programming' : 'プログラミング',
+        'javascript': 'ジャヴァスクリプト',
+        'java': 'ジャバ',
+        'cplus' : 'シープラスプラス',
+    },
+}
