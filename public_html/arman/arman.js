878ea0d603ec2a22f4586243c421e238bcccbd5f
russ <russjohnson09@gmail.com>
2014-07-27 22:15:58 -0500

    table view added

--- public_html/arman/arman.js
+++ public_html/arman/arman.js
@@ -2,7 +2,7 @@ var payroll = angular.module('payroll', []);
 
 payroll.controller('PayrollCtrl', function ($scope) {
   $scope.employees = [
-    {name:'Greg',hourly:10,paytype:'hourly'},
-    {name:'Mark',hourly:40,paytype:'salary'}
+    {name:'Greg',hourly:10,paytype:'Hourly'},
+    {name:'Mark',hourly:40,paytype:'Salary'}
   ];
 });
